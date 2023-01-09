a94537edcf4d119a0a7829fed13df4660b57de0b
Bertus Kruger <krugercreations@gmail.com>
2023-01-09 20:23:44 +1300

    Add files via upload

---
+++ odoo/addons/iot/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "IOT",
+    "version": "16.0",
+    "summary": """IOT""",
+    "license": "LGPL-3",
+    "depends": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "data" : [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ]
+}
