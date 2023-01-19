60624b7e5b11410f9d6e1411c57d9165eb0092bd
Bertus Kruger <krugercreations@gmail.com>
2023-01-19 21:30:46 +1300

    Update main.py

--- sensor/battery/main.py
+++ sensor/battery/main.py
@@ -1,12 +1,9 @@
-import machine, neopixel
-import time
-from machine import UART
-from machine import Pin
-
+import machine, neopixel , time
+from machine import UART,ADC,Pin
 
 ## LED
 np = neopixel.NeoPixel(machine.Pin(2), 1)
-np[0] = (0, 5, 0)  # set to blue, quarter brightness
+np[0] = (0, 5, 0) 
 np.write()
 
 
@@ -23,6 +20,7 @@ p0 = Pin(8, mode=Pin.OUT , pull = Pin.PULL_UP,value=1)
 p0.on()
 time.sleep(0.1)
 
+
 np[0] = (0, 0, 0) 
 np.write()
 time.sleep(1)
@@ -31,33 +29,42 @@ time.sleep(1)
 
 
 
-from machine import ADC,Pin
-import time
+
 p5 = Pin(5,Pin.IN)
-#p5 = Pin(5,Pin.IN,Pin.PULL_DOWN)
 adc = ADC(p5,atten=ADC.ATTN_11DB)
-#adc = ADC(p5)
 
-# avg the reading
+
+
+
+## avg the reading
+#calibration = 0.0054245
+#values = [0,0,0,0,0,0,0,0,0,0]
+#while True:
+#    np[0] = (50, 0, 0) 
+#    np.write()
+
+#    values.pop(0)
+#    values.append(adc.read())
+#    #print(sum(values)/len(values))
+#    voltage = sum(values)/len(values) * calibration
+#    voltage = round(voltage , 2)
+#    print(str(voltage) + 'V : ' + str(adc.read())) 
+#    #machine.deepsleep(500)
+    
+#    #transmit
+#    uart.write(str(voltage) + 'V : ' + str(adc.read()))
+    
+#    np[0] = (0, 0, 0) 
+#   np.write()
+
+    
+#    time.sleep(60)
+
 calibration = 0.0054245
-values = [0,0,0,0,0,0,0,0,0,0]
-while True:
-    np[0] = (50, 0, 0) 
-    np.write()
+adc_value = adc.read()
+voltage = round(adc_value * calibration , 2)
+print(str(voltage) + 'V : ' + str(adc_value) )  
+uart.write(str(voltage) + 'V : ' + str(adc_value) )
 
-    values.pop(0)
-    values.append(adc.read())
-    #print(sum(values)/len(values))
-    voltage = sum(values)/len(values) * calibration
-    voltage = round(voltage , 2)
-    print(str(voltage) + 'V : ' + str(adc.read())) 
-    #machine.deepsleep(500)
-    
-    #transmit
-    uart.write(str(voltage) + 'V : ' + str(adc.read()))
-    
-    np[0] = (0, 0, 0) 
-    np.write()
 
-    
-    time.sleep(60)
+machine.deepsleep(60000)
