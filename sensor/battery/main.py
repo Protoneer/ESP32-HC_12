3a6480b8762c7ba6a4b0acf5144a3fb0f66efa5e
Bertus Kruger <krugercreations@gmail.com>
2023-02-15 14:19:52 +1300

    Update main.py

--- sensor/battery/main.py
+++ sensor/battery/main.py
@@ -1,6 +1,12 @@
 import machine, neopixel , time
 from machine import UART,ADC,Pin
 
+# Run settings
+on_start_print_radio_settings = False
+deep_sleep_mode = True
+transmit_interval = 10
+
+
 ## LED
 np = neopixel.NeoPixel(machine.Pin(2), 1)
 np[0] = (0, 5, 0) 
@@ -8,24 +14,77 @@ np.write()
 
 
 
-print("Start Serial")
-uart = UART(1, baudrate=9600, tx=6, rx=7)
-uart.init(9600, bits=8, parity=None, stop=1)
-uart.any()
-uart.read()
+    
+    
 
-# Transmit mode
-print("Transmit mode")
-p0 = Pin(8, mode=Pin.OUT , pull = Pin.PULL_UP,value=1)
-p0.on()
+print("Start Serial")
+radio_uart = UART(1, baudrate=9600, tx=6, rx=7)
+radio_uart.init(9600, bits=8, parity=None, stop=1)
+radio_uart.any()
+radio_uart.read()
+
+radio_set_pin = Pin(8, mode=Pin.OUT , pull = Pin.PULL_UP,value=1)
+
+def radio_settings(set_pin, uart):
+    result = "Get Radio Config: "
+    
+    set_pin.off()
+    time.sleep(0.1)
+
+    uart.write('AT+V\r')
+    time.sleep(0.1)
+
+    uart.write('AT+RX\r')
+    time.sleep(0.2)
+    
+    result = radio_uart.read()
+    
+    set_pin.on()
+    time.sleep(0.1)
+    return result
+
+def radio_sleep(set_pin, uart):
+    result = "Set radio to sleep: "
+    set_pin.off()
+    time.sleep(0.1)
+
+    uart.write('AT+SLEEP\r')
+    time.sleep(0.1)
+
+    result = radio_uart.read()
+    time.sleep(0.1)
+
+    set_pin.on()
+    time.sleep(0.1)
+    return result
+    
+
+def radio_wake(set_pin, uart):
+    result = "Wakeup radio."
+    set_pin.off()
+    time.sleep(0.1)
+    set_pin.on()
+    time.sleep(0.1)
+    return result
+
+
+
+if on_start_print_radio_settings:
+    print(radio_settings(radio_set_pin,radio_uart))
+
+#print(radio_sleep(radio_set_pin,radio_uart))
+#print(radio_wake(radio_set_pin,radio_uart))
+
+
+# transmit mode
+radio_set_pin.on()
 time.sleep(0.1)
+
 
 
 np[0] = (0, 0, 0) 
 np.write()
 time.sleep(1)
-
-
 
 
 
@@ -36,35 +95,21 @@ adc = ADC(p5,atten=ADC.ATTN_11DB)
 
 
 
-## avg the reading
-#calibration = 0.0054245
-#values = [0,0,0,0,0,0,0,0,0,0]
-#while True:
-#    np[0] = (50, 0, 0) 
-#    np.write()
 
-#    values.pop(0)
-#    values.append(adc.read())
-#    #print(sum(values)/len(values))
-#    voltage = sum(values)/len(values) * calibration
-#    voltage = round(voltage , 2)
-#    print(str(voltage) + 'V : ' + str(adc.read())) 
-#    #machine.deepsleep(500)
-    
-#    #transmit
-#    uart.write(str(voltage) + 'V : ' + str(adc.read()))
-    
-#    np[0] = (0, 0, 0) 
-#   np.write()
-
-    
-#    time.sleep(60)
-
-calibration = 0.0054245
-adc_value = adc.read()
-voltage = round(adc_value * calibration , 2)
-print(str(voltage) + 'V : ' + str(adc_value) )  
-uart.write(str(voltage) + 'V : ' + str(adc_value) )
+def read_voltage():
+    calibration = 0.0054245
+    adc_value = adc.read()
+    voltage = round(adc_value * calibration , 2)
+    print(str(voltage) + 'V : ' + str(adc_value) )
+    return str(voltage) + 'V : ' + str(adc_value)
 
 
-machine.deepsleep(60000)
+if deep_sleep_mode:
+    print(radio_wake(radio_set_pin,radio_uart))
+    radio_uart.write(read_voltage())
+    print(radio_sleep(radio_set_pin,radio_uart))
+    machine.deepsleep(transmit_interval * 1000)
+else:
+    while True:
+        radio_uart.write(read_voltage())
+        time.sleep(transmit_interval)
