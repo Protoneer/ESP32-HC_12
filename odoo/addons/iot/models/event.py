9c5c91a6066b6bacc3535b05798994e6b3210e41
Bertus Kruger <krugercreations@gmail.com>
2023-04-25 11:56:50 +1200

    Update event.py

--- odoo/addons/iot/models/event.py
+++ odoo/addons/iot/models/event.py
@@ -8,3 +8,4 @@ class iot_event(models.Model):
     active = fields.Boolean(default=True)
     device_id = fields.Many2one(comodel_name='iot.device', string='Device' , required = "1")
     data = fields.Text("Data" , help='Data associated with the event')
+    data_binary = fields.Binary(attachment=False)
